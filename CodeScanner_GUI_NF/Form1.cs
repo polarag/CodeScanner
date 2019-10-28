86c64b004c0bbf60b0b6eb02b0536e5f3e552911
Giovanni Mounir <giovannimounir@gmail.com>
2019-10-28 17:47:12 +0200

    Switch to .NET Framework

--- CodeScanner_GUI_NF/Form1.cs
+++ CodeScanner_GUI_NF/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CodeScanner_GUI
+namespace CodeScanner_GUI_NF
 {
     public partial class Form1 : Form
     {
@@ -16,6 +16,5 @@ namespace CodeScanner_GUI
         {
             InitializeComponent();
         }
-
     }
 }
